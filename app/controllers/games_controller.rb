0a32f6ac439dd74cacfe9af63bfad741be9054d8
miyuki-muneishi <kyoruni@gmail.com>
2019-09-22 16:10:51 +0900

    #8 新規登録、検索のルーティング追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to :root
+  end
+
+  def search
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
