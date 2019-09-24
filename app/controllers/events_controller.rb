4b959c73eb9469a0dce1efaed074b7f44763c330
miyuki-muneishi <kyoruni@gmail.com>
2019-09-24 12:36:08 +0900

    #19 イベント編集、更新処理を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,17 @@ class EventsController < ApplicationController
     redirect_to game_path(id: game_params[:game_id])
   end
 
+  def edit
+    @game  = Game.find(params[:game_id])
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update(event_params)
+    redirect_to game_events_path(id: game_params[:game_id])
+  end
+
   private
   def event_params
     params.require(:event).permit(:title, :start, :end).merge(game_id: params[:game_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'games#index'
 
   resources  :games,  except: [:destroy] do
-    resources :events, only: [:index, :new, :create]
+    resources :events, only: [:index, :new, :create, :edit, :update]
     collection do
       get 'search'
     end
