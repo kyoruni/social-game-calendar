403eaf9c91d3fc6fd5470e3d4533073ea75d35e2
miyuki-muneishi <kyoruni@gmail.com>
2019-09-25 13:43:26 +0900

    #25 ユーザーの個別ページで、カレンダーを表示するajaxを作成

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -33,6 +33,42 @@ $(function() {
         .fail(function(event) {
           alert("ng");
         });
+      // ユーザーのマイページを表示した場合
+      //  ログインしているかは、ログアウトボタンがあるかどうかで判定
+    } else if (location.pathname == "/" && $("#log-out").length !== 0) {
+      $.ajax({
+        type: "GET",
+        url: window.location.href, // 通信先のURL
+        dataType: "json"
+      })
+        .done(function(event) {
+          if (event.length !== 0) {
+            $("#calendar").fullCalendar({
+              lang: "ja",
+              timeFormat: "HH:mm", // 24時間表記にする
+              header: {
+                left: "month,agendaWeek,agendaDay",
+                center: "title",
+                right: "prev,next today"
+              },
+              displayEventEnd: {
+                // 終了時間を表示
+                month: true,
+                basicWeek: false,
+                default: true
+              },
+              events: event // 受け取ったjsonの中身全部表示
+            });
+          }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          alert("ng!!");
+          console.log("ajax通信に失敗しました");
+          console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
+          console.log("textStatus     : " + textStatus); // タイムアウト、パースエラー
+          console.log("errorThrown    : " + errorThrown.message); // 例外情報
+          console.log("URL            : " + url);
+        });
     }
   });
 });
