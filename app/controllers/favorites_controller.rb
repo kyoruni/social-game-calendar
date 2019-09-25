b2283e19ca429b3bc7543ea1cda933a7f755359f
miyuki-muneishi <kyoruni@gmail.com>
2019-09-25 14:17:27 +0900

    #26 非同期でお気に入り登録できるように修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class FavoritesController < ApplicationController
+  # 非ログイン時はgameのindexに飛ばす
+  before_action :move_to_index
+
   def index
     @favorites = current_user.games
   end
 
   def create
-    game = Game.find(params[:game_id])
-    current_user.add_like(game)
-    redirect_to root_path
+    @game = Game.find(params[:game_id])
+    current_user.add_like(@game)
   end
 
   def destroy
-    game = Game.find(params[:game_id])
-    current_user.del_like(game)
-    redirect_to root_path
+    @game = Game.find(params[:game_id])
+    current_user.del_like(@game)
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :show, id: params[:id] unless user_signed_in?
   end
 end
