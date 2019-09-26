1debe9317b68dae166133ac3edadbc47b595cd5c
miyuki-muneishi <kyoruni@gmail.com>
2019-09-26 14:27:53 +0900

    #27 背景色はidで指定するように変更

--- db/fixtures/01_Game.rb
+++ db/fixtures/01_Game.rb
@@ -1,83 +1,95 @@
+color_id = Color.find_by(name: "水色").id
 Game.seed do |s|
   s.id        = 1
   s.name      = "Fate/Grand Order"
-  s.color     = "#56aae4"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "ピンク").id
 Game.seed do |s|
   s.id        = 2
   s.name      = "マギアレコード 魔法少女まどか☆マギカ外伝"
-  s.color     = "#ff99cc"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "濃いピンク").id
 Game.seed do |s|
   s.id        = 3
   s.name      = "プリンセスコネクト！Re:Dive"
-  s.color     = "#ef367f"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "紫").id
 Game.seed do |s|
   s.id        = 4
   s.name      = "メギド72"
-  s.color     = "#9858fc"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "水色").id
 Game.seed do |s|
   s.id        = 5
   s.name      = "グランブルーファンタジー"
-  s.color     = "#37a3e6"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "緑").id
 Game.seed do |s|
   s.id        = 6
   s.name      = "どうぶつの森 ポケットキャンプ"
-  s.color     = "#4ab199"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "ピンク").id
 Game.seed do |s|
   s.id        = 7
   s.name      = "アイドルマスター シンデレラガールズ スターライトステージ"
-  s.color     = "#cce9fc"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "ピンク").id
 Game.seed do |s|
   s.id        = 8
   s.name      = "アイドルマスター ミリオンライブ！シアターデイズ"
-  s.color     = "#1495f8"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "ピンク").id
 Game.seed do |s|
   s.id        = 9
   s.name      = "アイドルマスター シャイニーカラーズ"
-  s.color     = "#66b6ee"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "水色").id
 Game.seed do |s|
   s.id        = 10
   s.name      = "テイルズ オブ ザ レイズ"
-  s.color     = "#275afd"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "緑").id
 Game.seed do |s|
   s.id        = 11
   s.name      = "ポケモンGO"
-  s.color     = "#7fbdba"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
 
+color_id = Color.find_by(name: "緑").id
 Game.seed do |s|
   s.id        = 12
   s.name      = "フェアリードール"
-  s.color     = "#7fbdba"
+  s.color_id  = color_id
   s.textColor = "#ffffff"
 end
