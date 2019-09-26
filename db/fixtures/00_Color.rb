99c1ecb3f6e0e93940b2ee1a00e060cdee745e58
miyuki-muneishi <kyoruni@gmail.com>
2019-09-26 14:26:57 +0900

    #27 colorsの初期データを追加

---
+++ db/fixtures/00_Color.rb
@@ -0,0 +1,43 @@
+color_id = 0
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#82cddd"
+  s.name      = "水色"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#37a3e6"
+  s.name      = "青"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#ff99cc"
+  s.name      = "ピンク"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#ef367f"
+  s.name      = "濃いピンク"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#9858fc"
+  s.name      = "紫"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#4ab199"
+  s.name      = "緑"
+end
