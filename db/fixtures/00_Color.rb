39657764d6f52c463bc6f04c90bd89291654f074
miyuki-muneishi <kyoruni@gmail.com>
2019-09-26 14:29:56 +0900

    #27 使える色を追加

--- db/fixtures/00_Color.rb
+++ db/fixtures/00_Color.rb
@@ -3,15 +3,8 @@ color_id = 0
 color_id += 1
 Color.seed do |s|
   s.id        = color_id
-  s.code      = "#82cddd"
-  s.name      = "水色"
-end
-
-color_id += 1
-Color.seed do |s|
-  s.id        = color_id
-  s.code      = "#37a3e6"
-  s.name      = "青"
+  s.code      = "#ec4346"
+  s.name      = "赤"
 end
 
 color_id += 1
@@ -31,6 +24,20 @@ end
 color_id += 1
 Color.seed do |s|
   s.id        = color_id
+  s.code      = "#fecd67"
+  s.name      = "オレンジ"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#ff6c38"
+  s.name      = "濃いオレンジ"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
   s.code      = "#9858fc"
   s.name      = "紫"
 end
@@ -38,6 +45,55 @@ end
 color_id += 1
 Color.seed do |s|
   s.id        = color_id
-  s.code      = "#4ab199"
+  s.code      = "#c3b9ea"
+  s.name      = "薄い紫"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#006a95"
+  s.name      = "青"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#49c5ce"
+  s.name      = "水色"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#82cddd"
+  s.name      = "薄い水色"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#95aa67"
   s.name      = "緑"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#bdd588"
+  s.name      = "黄緑"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#a69d9a"
+  s.name      = "灰色"
+end
+
+color_id += 1
+Color.seed do |s|
+  s.id        = color_id
+  s.code      = "#d7cbc9"
+  s.name      = "薄い灰色"
 end
