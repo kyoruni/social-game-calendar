e3ebfcfec5cf077cb55b669dbae6012804708e17
miyuki-muneishi <kyoruni@gmail.com>
2019-09-26 14:15:58 +0900

    #27 マイグレーションやり直し
    games color、textColorカラムを削除

--- db/migrate/20190924025628_add_color_to_games.rb
+++
@@ -1,6 +0,0 @@
-class AddColorToGames < ActiveRecord::Migration[5.2]
-  def change
-    add_column :games, :color, :string, null: false
-    add_column :games, :textColor, :string, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,6 @@ ActiveRecord::Schema.define(version: 2019_09_25_042636) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color", null: false
-    t.string "textColor", null: false
     t.index ["name"], name: "index_games_on_name", unique: true
   end
 
