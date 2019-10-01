7612eff00511fe97dfe80e1045490677758fda0d
miyuki-muneishi <kyoruni@gmail.com>
2019-10-01 19:36:48 +0900

    #28 ゲームの編集時、必須入力チェックを追加

--- app/assets/javascripts/game-save.js
+++ app/assets/javascripts/game-save.js
@@ -1,18 +1,31 @@
 $(document).on("turbolinks:load", function() {
-  // ボタン押下時、入力チェックする
-  //  未入力の場合、エラーテキストを表示
-  $("#game-save-btn").click(function() {
-    var errorMessage = "必須入力です！";
+  var errorMessage = "必須入力です！";
 
-    // タイトルの入力チェック
+  // タイトルの入力チェック
+  function name_input_check() {
     var input = $("#game_name").val();
     if (input == "" || !input.match(/[^\s\t]/)) {
       $(".required__text__name").text(errorMessage);
     }
-    // 色の選択チェック
+  }
+
+  // 色の選択チェック
+  function color_input_check() {
     var input_radio = $("input[name='game[color_id]']:checked").val();
     if (!input_radio) {
       $(".required__text__color").text(errorMessage);
     }
+  }
+
+  // 登録ボタン押下時、入力チェックする
+  $("#game-save-btn").click(function() {
+    name_input_check();
+    color_input_check();
+  });
+
+  // 更新ボタン押下時、入力チェックする
+  $("#game-update-btn").click(function() {
+    name_input_check();
+    color_input_check();
   });
 });
