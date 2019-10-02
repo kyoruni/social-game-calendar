ae78e3555d95c657dc2e7d86e1cdfd2142255f8c
miyuki-muneishi <kyoruni@gmail.com>
2019-10-03 01:08:46 +0900

    #46 共通部分をまとめる

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,25 @@
 class FavoritesController < ApplicationController
   # 非ログイン時はgameのindexに飛ばす
   before_action :move_to_index
+  before_action :set_game, only: [:create, :destroy]
 
   def index
     @favorites = current_user.games
   end
 
   def create
-    @game = Game.find(params[:game_id])
     current_user.add_like(@game)
   end
 
   def destroy
-    @game = Game.find(params[:game_id])
     current_user.del_like(@game)
   end
 
   private
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
   def move_to_index
     redirect_to action: :show, id: params[:id] unless user_signed_in?
   end
