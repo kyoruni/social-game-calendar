543bbbcb67214f584f59dd48ebc65c29cd4f4eb5
miyuki-muneishi <kyoruni@gmail.com>
2019-10-18 23:40:30 +0900

    #57 userモデル 入力チェックの単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,32 @@ describe User do
   describe '#create' do
     describe '入力チェック' do
       it "全ての項目が揃っていると登録できる" do
+        user = build(:user)
+        expect(user).to be_valid
       end
   
       it "名前が空の場合、登録できない" do
+        user = build(:user, name: "")
+        user.valid?
+        expect(user.errors[:name]).to include("を入力してください")
       end
   
       it "メールアドレスが空の場合、登録できない" do
+        user = build(:user, email: "")
+        user.valid?
+        expect(user.errors[:email]).to include("を入力してください")
       end
   
       it "パスワードが空の場合、登録できない" do
+        user = build(:user, password: "")
+        user.valid?
+        expect(user.errors[:password]).to include("を入力してください")
       end
 
       it "確認用パスワードが空の場合、登録できない" do
+        user = build(:user, password_confirmation: "")
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
       end
     end
 
